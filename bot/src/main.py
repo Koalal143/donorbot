5219da6d16799a1eace785c619e458ddf3b6e857
QuickFox43 <koalal1428@gmail.com>
2025-07-17 21:22:51 +0300

    refactor(bot): use correct di get for site

--- bot/src/main.py
+++ bot/src/main.py
@@ -8,6 +8,7 @@ from dishka import AsyncContainer
 
 from src.core.config import Settings
 from src.di.container import container
+from src.handlers import main_router
 from src.lifespan import on_shutdown, on_startup
 
 
@@ -18,24 +19,28 @@ async def setup_webhook(container: AsyncContainer) -> None:
     app: Application = await container.get(Application)
     webhook_requests_handler: SimpleRequestHandler = await container.get(SimpleRequestHandler)
     runner: AppRunner = await container.get(AppRunner)
-    site: TCPSite = await container.get(TCPSite)
 
-    if not settings.telegram_bot.webhook_url or not settings.telegram_bot.webhook_path:
-        msg = "webhook_url or webhook_path is not set"
+    if (
+        not settings.telegram_bot.webhook_url
+        or not settings.telegram_bot.webhook_path
+        or not settings.telegram_bot.webhook_secret_token
+    ):
+        msg = "webhook_url or webhook_path or webhook_secret_token is not set"
         raise ValueError(msg)
 
     await bot.set_webhook(
         settings.telegram_bot.webhook_url,
         allowed_updates=dp.resolve_used_update_types(),
-        secret_token=settings.telegram_bot.token.get_secret_value(),
+        secret_token=settings.telegram_bot.webhook_secret_token.get_secret_value(),
     )
 
     webhook_requests_handler.register(app, path=settings.telegram_bot.webhook_path)
     setup_application(app, dp, bot=bot)
 
     await runner.setup()
-    await site.start()
 
+    site: TCPSite = await container.get(TCPSite)
+    await site.start()
     await asyncio.Event().wait()
 
 
@@ -45,6 +50,7 @@ async def main() -> None:
     dp: Dispatcher = await container.get(Dispatcher)
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
+    dp.include_router(main_router)
 
     if settings.telegram_bot.use_webhook:
         await setup_webhook(container)
