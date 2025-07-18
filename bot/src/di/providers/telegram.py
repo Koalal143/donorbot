8a6e080cc4588ce7f49e4dda4c0e3010b33db70d
QuickFox43 <koalal1428@gmail.com>
2025-07-18 12:39:01 +0300

    feat(di): add redis storage provider

--- bot/src/di/providers/telegram.py
+++ bot/src/di/providers/telegram.py
@@ -1,11 +1,17 @@
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
 from dishka import Provider, Scope, provide
+from redis.asyncio import Redis
 
 from src.core.config import Settings
 
 
 class TelegramBotProvider(Provider):
     scope = Scope.APP
+
+    @provide
+    def get_redis_storage(self, redis: Redis) -> RedisStorage:
+        return RedisStorage(redis=redis)
 
     @provide
     def get_bot(self, settings: Settings) -> Bot:
