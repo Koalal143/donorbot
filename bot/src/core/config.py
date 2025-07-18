df37406ac0c9ce5149dff015553f2b4e2a47db67
QuickFox43 <koalal1428@gmail.com>
2025-07-18 08:33:20 +0300

    debug(config): add variable for test

--- bot/src/core/config.py
+++ bot/src/core/config.py
@@ -24,6 +24,8 @@ class PostgresConfig(BaseModel):
 
     @property
     def url(self) -> SecretStr:
+        a = f"postgresql+asyncpg://{self.user}:{self.password.get_secret_value()}@{self.host}:{self.port}/{self.db}"
+        print(a)  # noqa: T201
         return SecretStr(
             f"postgresql+asyncpg://{self.user}:{self.password.get_secret_value()}@{self.host}:{self.port}/{self.db}"
         )
